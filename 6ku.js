2157dbaaeeba7fbe7b062a463e3139e77dc3f5be
Mikhail8815 <mishutka150588@mail.ru>
2025-08-05 09:38:54 +0300

    Counting Duplicates

--- 6ku.js
+++ 6ku.js
@@ -184,3 +184,42 @@ function findOutlier(integers){
   }
 }
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Подсчитайте количество дубликатов
+// Напишите функцию, которая возвращает количество различных буквенно-цифровых символов (без учёта регистра), встречающихся во входной строке более одного раза. Можно предположить, что входная строка содержит только буквы (как заглавные, так и строчные) и цифры.
+
+// Пример
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "неделимость" -> 1 # 'i' occurs six times
+// "Неделимости" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+const a = 'aaAbBBc3'
+
+function duplicateCount(text){
+  const arr = text.split('')
+  const obj = {}
+  let count = 0
+  arr.forEach((el)=>{
+    if (obj[el.toLowerCase()]) {
+      obj[el.toLowerCase()]++
+    } else {
+      obj[el.toLowerCase()] = 1
+    }
+  })
+
+  for (let key in obj) {
+    if (obj[key] > 1) {
+      count++
+    }
+  }
+  return count
+}
+ console.log(duplicateCount(a))
+
+ //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
